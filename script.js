65c435a9a802717378e2cc92b0a7ed0bcbd01deb
Neurtal <115718420+Neurtal@users.noreply.github.com>
2026-07-11 02:09:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const clickSound=new Audio("https://actions.google.com/sounds/v1/cartoon/pop.ogg");
+
 const prizes = [
 1,
 5,
@@ -33,7 +35,8 @@ const status = document.getElementById("status");
 const bankButton = document.getElementById("bankButton");
 
 let selectedBox = null;
-let opened = 0;
+let round = 1;
+let target = 6;
 
 drawMoney();
 drawBoxes();
@@ -100,9 +103,14 @@ return;
 
 }
 
+clickSound.currentTime=0;
+clickSound.play();
 element.classList.add("open");
 
-element.innerText=values[index].toLocaleString("tr-TR")+" ₺";
+element.innerHTML=
+"<div style='font-size:18px'>"+
+values[index].toLocaleString("tr-TR")+
+"<br>₺</div>";
 
 const prize=document.getElementById("money-"+values[index]);
 
@@ -116,11 +124,17 @@ prize.style.textDecoration="line-through";
 
 opened++;
 
-status.innerText=opened+" kutu açıldı.";
+status.innerText=
+opened+
+" kutu açıldı. "+
+(target-opened)+
+" kutu daha aç.";
 
-if(opened%5===0){
+if(opened>=target){
 
 bankButton.disabled=false;
+
+status.innerText="🏦 Bankadan teklif geldi!";
 
 }
 
@@ -161,6 +175,49 @@ offer.toLocaleString("tr-TR")+" ₺\n\n"+
 );
 
 bankButton.disabled=true;
+    round++;
+
+switch(round){
+
+case 2:
+target=11;
+break;
+
+case 3:
+target=14;
+break;
+
+case 4:
+target=16;
+break;
+
+case 5:
+target=18;
+break;
+
+case 6:
+target=19;
+break;
+
+case 7:
+target=20;
+break;
+
+case 8:
+target=21;
+break;
+
+case 9:
+target=22;
+break;
+
+case 10:
+target=23;
+break;
+
+}
+
+status.innerText="Oyuna devam ediliyor...";
 
 if(answer){
 
@@ -176,13 +233,85 @@ function finishGame(){
 
 const ownPrize = values[selectedBox];
 
+let answer = confirm(
+"🎁 Son kutuya geldin!\n\n"+
+"Kendi kutunu açmak ister misin?\n\n"+
+"Tamam = Aç\n"+
+"İptal = Oyunu yeniden başlat"
+);
+
+if(!answer){
+
+location.reload();
+
+return;
+
+}
+
+const allBoxes=document.querySelectorAll(".box");
+
+allBoxes[selectedBox].classList.add("open");
+
+allBoxes[selectedBox].innerHTML=
+"<div style='font-size:18px'>"+
+ownPrize.toLocaleString("tr-TR")+
+"<br>₺</div>";
+
+setTimeout(()=>{
+
+document.body.innerHTML=`
+
+<div style="display:flex;
+justify-content:center;
+align-items:center;
+height:100vh;
+flex-direction:column;
+background:#09111f;
+color:white;
+font-family:Arial">
+
+<h1 style="font-size:55px;color:gold;">
+🎉 Tebrikler
+</h1>
+
+<h2>
+
+${ownPrize.toLocaleString("tr-TR")} ₺
+
+</h2>
+
+<button onclick="location.reload()"
+style="
+margin-top:40px;
+padding:20px 50px;
+font-size:22px;
+cursor:pointer;
+border:none;
+border-radius:12px;
+background:gold;">
+
+Tekrar Oyna
+
+</button>
+
+</div>
+
+`;
+
+},700);
+
+}
+
+const ownPrize = values[selectedBox];
+
 const allBoxes = document.querySelectorAll(".box");
 
 allBoxes[selectedBox].classList.add("open");
 allBoxes[selectedBox].innerText = ownPrize.toLocaleString("tr-TR")+" ₺";
 
 setTimeout(()=>{
-
+    
+document.body.style.background="#064420";
 alert(
 "Tebrikler!\n\n"+
 "Kendi kutunda\n"+
